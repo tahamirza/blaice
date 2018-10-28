b9a64116f8c86cb5755293bde5dc051145812ee0
Taha Mirza <mail@tahamirza.com>
2018-10-28 17:27:00 -0400

    Lex one token!

--- blaice.pas
+++ blaice.pas
@@ -1,11 +1,45 @@
 {A Pascal compiler written in Pascal :)}
 program blaice;
-var ch : char;
+
+type
+   string    = array[0..255] of char;
+   tokentype =  (identifier, number);
+   token     = record
+                  line, column : integer;
+                  name         : tokentype;
+                  text         : string;
+               end;
+
+var nextToken : token;
+
+procedure getToken();
+var
+   ch   : char;
+   done : boolean;
+   i    : integer;
 begin
-   while not eof do
-      begin
-         ch := input^;
-         get(input);
-         write(ch);
-      end
+   done := false;
+   i := 0;
+   while not done do
+   begin
+      ch := input^;
+      get(input);
+      
+      case ch of
+        'a'..'z':
+        begin
+           nextToken.name := identifier;
+           nextToken.text[i] := ch;
+        end;
+        {whitespace}
+        ' ',chr(13),chr(10): done := true;
+      end;
+
+      i := i + 1;
+   end;
+end;
+
+begin
+   getToken;
+   writeln(nextToken.text);
 end .
